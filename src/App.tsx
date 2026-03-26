125a1467a3969fdb74b872a4c4e31cc12621221b
Israfil Guseinov <israfil.guseynov.98@gmail.com>
2026-03-25 21:31:20 -0700

    chore: lazy load pages and vendor chunk splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes, useLocation } from 'react-router';
 
 import { ProtectedRoute, PublicRoute } from './components/auth';
-import { ScrollToTop } from './components/common';
+import { LoadingSpinner, ScrollToTop } from './components/common';
 import { Footer, Navbar } from './components/layout';
-import {
-  AdminDashboardPage,
-  AdminMenuPage,
-  BookingPage,
-  ConfirmationPage,
-  HomePage,
-  LoginPage,
-  MenuPage,
-} from './pages';
+
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const MenuPage = lazy(() => import('./pages/MenuPage/MenuPage'));
+const BookingPage = lazy(() => import('./pages/BookingPage/BookingPage'));
+const ConfirmationPage = lazy(
+  () => import('./pages/ConfirmationPage/ConfirmationPage'),
+);
+const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage'));
+const AdminDashboardPage = lazy(
+  () => import('./pages/AdminDashboardPage/AdminDashboardPage'),
+);
+const AdminMenuPage = lazy(() => import('./pages/AdminMenuPage/AdminMenuPage'));
 
 const App = () => {
   const { pathname } = useLocation();
@@ -22,21 +26,26 @@ const App = () => {
       <ScrollToTop />
       <Navbar />
       <main>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/menu" element={<MenuPage />} />
-          <Route path="/book" element={<BookingPage />} />
-          <Route path="/booking-confirmation" element={<ConfirmationPage />} />
+        <Suspense fallback={<LoadingSpinner fullScreen />}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/menu" element={<MenuPage />} />
+            <Route path="/book" element={<BookingPage />} />
+            <Route
+              path="/booking-confirmation"
+              element={<ConfirmationPage />}
+            />
 
-          <Route element={<PublicRoute />}>
-            <Route path="/login" element={<LoginPage />} />
-          </Route>
+            <Route element={<PublicRoute />}>
+              <Route path="/login" element={<LoginPage />} />
+            </Route>
 
-          <Route element={<ProtectedRoute />}>
-            <Route path="/admin" element={<AdminDashboardPage />} />
-            <Route path="/admin/menu" element={<AdminMenuPage />} />
-          </Route>
-        </Routes>
+            <Route element={<ProtectedRoute />}>
+              <Route path="/admin" element={<AdminDashboardPage />} />
+              <Route path="/admin/menu" element={<AdminMenuPage />} />
+            </Route>
+          </Routes>
+        </Suspense>
       </main>
       {!isAdminRoute && <Footer />}
     </div>
